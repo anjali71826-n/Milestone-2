66c01dd20f34ff679a7af6395cc080c63c9e7151
anjshva7 <anjali71826@gmail.com>
2025-12-01 23:10:37 +0530

    Update email.ts

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -2,9 +2,7 @@ import nodemailer from "nodemailer";
 
 export async function sendEmail(to: string, content: any) {
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || "587"),
-    secure: process.env.SMTP_SECURE === "true", // true for SSL (465), false for TLS (587)
+    service: "gmail",
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASSWORD,
